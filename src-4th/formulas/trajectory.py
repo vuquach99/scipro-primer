bdbf9d07ddc7e004adb749db159d8a6790d0a223
Vu Quach <hvq20@cam.ac.uk>
2019-03-17 17:05:26 +0000

    updated chapter 1

--- src-4th/formulas/trajectory.py
+++ src-4th/formulas/trajectory.py
@@ -4,12 +4,12 @@ theta = 60  # degrees
 x = 0.5     # m
 y0 = 1      # m
 
-print """\
+print ("""\
 v0    = %.1f km/h
 theta = %d degrees 
 y0    = %.1f m
 x     = %.1f m\
-""" % (v0, theta, y0, x)
+""" % (v0, theta, y0, x))
 
 from math import pi, tan, cos
 # Convert v0 to m/s and theta to radians
@@ -18,5 +18,5 @@ theta = theta*pi/180
 
 y = x*tan(theta) - 1/(2*v0**2)*g*x**2/((cos(theta))**2) + y0
 
-print 'y     = %.1f m' % y
+print ('y     = %.1f m' % y)
 
